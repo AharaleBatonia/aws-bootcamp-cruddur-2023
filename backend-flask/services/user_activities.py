6e00d4290549ae6b822212d56f17fb71f14871cc
Aharale Batonia <aharaleb@gmail.com>
2023-03-06 16:01:44 +0100

    second try - XRAY subsegment
    second try - XRAY subsegment - looks like it is working.

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -6,34 +6,41 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     
-    # adding XRAY subsegment 1. copy the line bellow, 2. changing < in_segment('segment_name') > with < in_segment('home.activities') > 
-      # 3. indenting everything bellow 1 tab to the right. 
-      # 
-    with xray_recorder.in_segment('home.activities') as segment:
-      model = {
-        'errors': None,
-        'data': None
-      }
+    # adding XRAY subsegment 1. copy the line bellow, 2. changing < in_segment('segment_name') > with < in_segment('user_activities') > 
+      # 3. indenting back to the same level everything bellow 1 tab to the right. 4. add subsegment 5. change ... in_segment('subsegment_name') to in_segment('mock-data')
+      #    
+    segment = xray_recorder.begin.segment('user_activities')
+    model = {
+      'errors': None,
+      'data': None
+    }
 
-      now = datetime.now(timezone.utc).astimezone()
+    now = datetime.now(timezone.utc).astimezone()
+
+    
+
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      now = datetime.now()
+      results = [{
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'handle':  'Andrew Brown',
+        'message': 'Cloud is fun!',
+        'created_at': (now - timedelta(days=1)).isoformat(),
+        'expires_at': (now + timedelta(days=31)).isoformat()
+      }]
+      model['data'] = results
       
-      # adding XRAY subsegment +++ adding dict 
-      dict = {
-          "now": now.isoformat()
-      }
-      # adding XRAY subsegment
-      segment.put_metadata('key', dict, 'namespace')
 
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-        results = [{
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=1)).isoformat(),
-          'expires_at': (now + timedelta(days=31)).isoformat()
-        }]
-        model['data'] = results
-      return model
+    # adding XRAY subsegment
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+      # adding XRAY subsegment +++ adding dict 
+    dict = {
+        "now": now.isoformat(),
+        "results-size": len(model['data'])
+    }
+    # adding XRAY subsegment
+    subsegment.put_metadata('key', dict, 'namespace')
+    
+    return model
