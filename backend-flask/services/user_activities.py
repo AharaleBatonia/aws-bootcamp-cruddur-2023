692bd75c4b3768be4585d00cc5b87289b37a19ed
Aharale Batonia <aharaleb@gmail.com>
2023-03-06 15:35:45 +0100

    failed attempt to create XRAY subsegment
    first ... failed attempt to create XRAY subsegment

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,23 +1,39 @@
 from datetime import datetime, timedelta, timezone
+
+# adding XRAY subsegment +++ as a respons to an error 
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
-    model = {
-      'errors': None,
-      'data': None
-    }
+    
+    # adding XRAY subsegment 1. copy the line bellow, 2. changing < in_segment('segment_name') > with < in_segment('home.activities') > 
+      # 3. indenting everything bellow 1 tab to the right. 
+      # 
+    with xray_recorder.in_segment('home.activities') as segment:
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    now = datetime.now(timezone.utc).astimezone()
+      now = datetime.now(timezone.utc).astimezone()
+      
+      # adding XRAY subsegment +++ adding dict 
+      dict = {
+          "now": now.isoformat()
+      }
+      # adding XRAY subsegment
+      segment.put_metadata('key', dict, 'namespace')
 
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
-    return model
+      if user_handle == None or len(user_handle) < 1:
+        model['errors'] = ['blank_user_handle']
+      else:
+        now = datetime.now()
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
+      return model
